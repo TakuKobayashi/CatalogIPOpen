4628245ae6160832466bc89be337106c62513614
nano06126728 <nano.asm@gmail.com>
2015-11-07 11:51:14 +0900

    スタンダードアセット追加

---
+++ Assets/Scripts/GameInstanceData.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class GameInstanceData : MonoBehaviour {
+	public class GameInstanceDataHold
+	{
+		public float m_DataA;
+
+	}
+	public static GameInstanceDataHold m_GameInstanceData;
+
+
+	public float m_DataB;
+	// Use this for initialization
+	void Start()
+	{
+		if (m_GameInstanceData == null)
+		{
+			m_GameInstanceData = new GameInstanceDataHold();
+		}
+	}
+
+	GameInstanceDataHold GetGrobal()
+	{
+		if (m_GameInstanceData == null)
+		{
+			m_GameInstanceData = new GameInstanceDataHold();
+		}
+
+		return m_GameInstanceData;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			m_GameInstanceData.m_DataA = 120f;
+			Application.LoadLevel("sceneB");
+		}
+
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			m_DataB = m_GameInstanceData.m_DataA;
+		}
+	}
+}
