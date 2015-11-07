a6eebf50eb0acf3a670e1f56485110db1b18d3f0
YuukiHirai <kd1172491@st.kobedenshi.ac.jp>
2015-11-07 13:48:21 +0900

    仮

--- Assets/Scripts/Camera_Player.cs
+++ Assets/Scripts/Camera_Player.cs
@@ -11,19 +11,8 @@ public class Camera_Player : MonoBehaviour
 
     void Update()
     {
-        //transform.position = target.localPosition - new Vector3(0.0f, 0.0f, 24.0f);
-        //transform.LookAt(target);
-
-        Vector3 vec = target.localPosition;
-        Vector3 fvec = target.forward;
-
-        vec.z -= 24.0f;
-        fvec *= 6.0f;
-        //fvec *= 8.0f + Trans.JetBoard_Speed;
-        //fvec.z = 1f;
-
-
-        transform.position = vec - fvec;
-        transform.LookAt(target.localPosition);
+        transform.position = target.localPosition - new Vector3(0.0f, 0.0f, 24.0f);
+        transform.LookAt(target);
+ 
     }
 }
