f3d801852d41e2a186794e95e970f40ab5db9b1d
nano06126728 <nano.asm@gmail.com>
2015-11-07 22:45:46 +0900

    タイトル追加

--- Assets/Scripts/SceneCtrlTitle.cs
+++ Assets/Scripts/SceneCtrlTitle.cs
@@ -3,18 +3,44 @@ using System.Collections;
 
 public class SceneCtrlTitle : MonoBehaviour {
 
-	GameInstanceData.HoldValue m_HoldValue;
+	public AnimationCurve m_Curve;
+	public float m_OffsetSpeed;
+	public float m_MaxSpeed;
+	public float m_OutTime;
+
+	enum STATE {
+		BLANK,
+		WAIT,
+		MOVE,
+		END
+	}
+
+	STATE m_State;
+	STATE m_StateOld;
+	bool m_EnterPls;
 
 	// Use this for initialization
 	void Start () {
-		m_HoldValue = GameInstanceData.GetGrobal();
-		m_HoldValue.m_NowStage = 1;
-		m_HoldValue.m_Score = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		if (m_StateOld != m_State)
+		{
+			m_StateOld = m_State;
+			m_EnterPls = true;
+		}
+		else
+		{
+			m_EnterPls = false;
+		}
+
+		switch (m_State)
+		{
+			case ():
+				break;
+		}
 	}
 }
