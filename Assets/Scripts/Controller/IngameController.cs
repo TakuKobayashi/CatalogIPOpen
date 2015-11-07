932ded0dc4af9a8b6b55f1c6a370f7f85f6efb02
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-07 17:01:21 +0900

    制限時間が画面上に出る

---
+++ Assets/Scripts/Controller/IngameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class IngameController : SingletonBehaviour<IngameController> 
+{
+	[SerializeField] Prefab timerCounter;
+	
+	public void Initialize()
+	{
+		GameObject go = Util.InstantiateTo (this.gameObject, timerCounter);
+		go.GetComponent<TimeCounter>().onComplete = FinishGame;
+	}
+
+	void FinishGame(){
+		// end of game
+		Debug.Log("complete");
+	}
+}
