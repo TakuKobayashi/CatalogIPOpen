7d7d4fae71b3ce6018894456f6bca1a7c42b153c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-07 10:12:24 +0900

    大事なものだけ隠すようなフォルダの配置をした

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    Vector3 Player_Vec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Player_Vec += new Vector3(0.0f, 0.0f, 1.0f);
+        transform.localPosition = Player_Vec;
+    }
+}
