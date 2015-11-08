5114d47406f1371843d8f69f7a44605bde7ebb8f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-08 16:52:42 +0900

    startTImerを実装した

--- Assets/Scripts/TimeCounter.cs
+++ Assets/Scripts/TimeCounter.cs
@@ -9,13 +9,22 @@ public class TimeCounter : MonoBehaviour{
 	public Action onComplete;
 	public long limitTimeSecond;
 	bool isFinish;
+	bool isStart;
 	[SerializeField] Text remainTimeText;
 	
 	public void Inithialize(){
 		startTime = DateTime.Now;
+		TimeSpan time = TimeSpan.FromSeconds (limitTimeSecond);
+		remainTimeText.text = string.Format ("{0}:{1}:{2}", time.Minutes, time.Seconds, time.Milliseconds);
+	}
+
+	public void startTimer(){
+		startTime = DateTime.Now;
+		isStart = true;
 	}
 
 	void Update(){
+		if(!isStart) return;
 		TimeSpan progress = DateTime.Now - startTime;
 		TimeSpan remainTime = TimeSpan.FromSeconds (limitTimeSecond) - progress;
 		if (remainTime <= TimeSpan.FromSeconds (0)) {
