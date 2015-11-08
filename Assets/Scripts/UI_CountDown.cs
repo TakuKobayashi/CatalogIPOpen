7bd9a3d5e1c972e693a26851098e7542fdf80319
YuukiHirai <kd1172491@st.kobedenshi.ac.jp>
2015-11-08 09:43:04 +0900

    スタート時のカウントダウン処理

---
+++ Assets/Scripts/UI_CountDown.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_CountDown : MonoBehaviour {
+    public Text Count;
+    int Fcnt=0;
+    // Use this for initialization
+    void Start () {
+        Fcnt = 0;
+        Count.text = "3";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Fcnt++;
+        if (Fcnt == 60) Count.text = "2";
+        if (Fcnt == 120) Count.text = "1";
+        if (Fcnt == 180) Count.text = "GO";
+        if (Fcnt == 200) Count.text = "";
+
+    }
+}
