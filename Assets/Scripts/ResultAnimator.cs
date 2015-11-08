84f6d5fed24cc121292721cdae6bef2060501647
nano06126728 <nano.asm@gmail.com>
2015-11-08 15:18:14 +0900

    リゾルトアニメーター 目開け化

--- Assets/Scripts/ResultAnimator.cs
+++ Assets/Scripts/ResultAnimator.cs
@@ -5,14 +5,32 @@ public class ResultAnimator : MonoBehaviour {
 
 	GameObject m_ResultChara;
 	Animator m_Anime;
+
+	UnityChan.FaceUpdate m_FaceUpdate;
+
+	float m_Time = 0.0f;
+	bool m_FaceUpEnd = false;
 	void Awake()
 	{
 		m_ResultChara = GameObject.Find("ValkyrieFaceAnimation");
 		m_Anime = m_ResultChara.GetComponent<Animator>();
+		m_FaceUpdate = m_ResultChara.GetComponent<UnityChan.FaceUpdate>();
     }
 
 	void Start()
 	{
+		
 		m_Anime.Play("003_NOT01_Final", -1 , 0.062f);
-    }
+
+	}
+
+	void Update()
+	{
+		m_Time += Time.deltaTime;
+		if (m_Time > 0.1f && m_FaceUpEnd == false)
+		{
+			m_FaceUpdate.OnCallChangeFace("default@unitychan");
+			m_FaceUpEnd = true;
+        }
+	}
 }
