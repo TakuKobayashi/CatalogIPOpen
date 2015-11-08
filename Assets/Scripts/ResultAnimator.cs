2224cb6969f2c9a70eae51ec779a1a1aecd35edf
nano06126728 <nano.asm@gmail.com>
2015-11-08 14:12:00 +0900

    リゾルトアニメーション

---
+++ Assets/Scripts/ResultAnimator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultAnimator : MonoBehaviour {
+
+	GameObject m_ResultChara;
+	Animator m_Anime;
+	void Awake()
+	{
+		m_ResultChara = GameObject.Find("ValkyrieFaceAnimation");
+		m_Anime = m_ResultChara.GetComponent<Animator>();
+    }
+
+	void Start()
+	{
+		m_Anime.Play("003_NOT01_Final" , -1 , 0.0032f);
+    }
+}
